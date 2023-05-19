cbfe2246378e0194d379bea825d471fb627a2bad
Gowtham <113886400+Gowthamkannappan@users.noreply.github.com>
2023-05-19 14:47:29 +0530

    Update urls.py

--- apps/transactions/urls.py
+++ apps/transactions/urls.py
@@ -4,5 +4,5 @@ urlpatterns = [
     path('',name="dashboard"),     
     path('add/<int:id>',name="add-transaction"),
     path('update/<int:id>',name='update-transaction'),
-    path('delete',name='delete-transaction'),                                         
+    path('delete/<int:id>',name='delete-transaction'),                                         
 ]
